041b6d58c4eb82777fa4ca878bbd4d07a02d2bfc
TheShivamChand <shivamdeopa@gmail.com>
2019-08-14 19:08:17 +0530

    update

--- app.py
+++ app.py
@@ -20,13 +20,11 @@ books=[
 }
 ]
 
-
 def validBookObject(bookObject):
 	if ("name" in bookObject and "price" in bookObject and "isbn" in bookObject):
 		return True
 	else:
 		return False
-
 
 @app.route('/')
 def hello_world():
