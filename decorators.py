7fca47cee9458ec97c9cc58b30654f51d59ae6d9
TheShivamChand <shivamdeopa@gmail.com>
2019-08-25 19:46:51 +0530

    decorators and wrappers

--- decorators.py
+++ decorators.py
@@ -3,6 +3,14 @@ def decorator_function(original_function):
 		return original_function(*args,**kwargs)
 	return wrapper_function
 
+class decorator_class(object):
+	def __init__(self,original_function):
+		self.original_function=original_function
+
+	def __call__(self,*args,**kwargs):
+		print('call method executed this before {}'.format(self.original_function.__name__))
+		return self.original_function(*args,**kwargs)
+
 
 @decorator_function
 def display():
@@ -13,3 +21,13 @@ display()
 def display_info(name,age):
 	print('display_info ran with arguments ({}, {})'.format(name,age))
 display_info('Shivam',21)
+
+@decorator_class
+def display():
+	print('display function ran with class too')
+display()
+
+@decorator_class
+def display_info(name,age):
+	print('display_info ran with arguments ({}, {})'.format(name,age))
+display_info('Satyam',19)
